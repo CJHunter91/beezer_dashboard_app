6eaa8480bc3c368ea4ca65da4d720e48a167e2f2
Chris Hunter <orkem2@gmail.com>
2017-10-14 13:54:19 +0100

    gets data back from db

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyCowc3qO-CUJwlL5Lo2R6Uk6kWFVcmrfHY",
+  authDomain: "beezer-test-cjhunter.firebaseapp.com",
+  databaseURL: "https://beezer-test-cjhunter.firebaseio.com",
+  projectId: "beezer-test-cjhunter",
+  storageBucket: "beezer-test-cjhunter.appspot.com",
+  messagingSenderId: "1085348013195"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+firebase.database().ref('accounts/').once('value').then(function(snapshot) {
+  console.log(snapshot.val())
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
